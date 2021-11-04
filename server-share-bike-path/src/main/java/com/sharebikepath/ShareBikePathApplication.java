d82440070a99679a670e1e4658e55e8fec4c1d63
Michał Maciocha <nt.michal@gmail.com>
2021-11-04 19:15:49 +0100

    backend-init

---
+++ server-share-bike-path/src/main/java/com/sharebikepath/ShareBikePathApplication.java
@@ -0,0 +1,13 @@
+package com.sharebikepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareBikePathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareBikePathApplication.class, args);
+    }
+
+}
---
+++ server-share-bike-path/src/test/java/com/sharebikepath/ShareBikePathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharebikepath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareBikePathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
